9fd2b1f3b4bb8c26b9847614efcc64e8001956b3
patiname <patiname@naver.com>
2021-08-02 19:37:06 +0900

    header

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -1,4 +1,18 @@
+import styled from "styled-components";
+import { Section } from "../../components/Section";
+
+const Main = styled.section`
+  width: 100%;
+  height: 900px;
+  background-color: #333;
+`;
 
 export const Home = () => {
-    return <div>Home</div>
-}
+  return (
+    <div>
+      <Main></Main>
+
+      <Section>홈 컨텐츠</Section>
+    </div>
+  );
+};
