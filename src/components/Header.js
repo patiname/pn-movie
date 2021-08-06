83be031128c592a4f10651972d0f6268ffa04fbf
patiname <patiname@naver.com>
2021-08-06 18:22:12 +0900

    header event

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,52 +1,68 @@
-import { Link } from "react-router-dom"
-import styled from "styled-components"
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const SHeader = styled.header`
-    width:100%;
-    height:60px;
-    padding: 0 80px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    position: fixed;
-    top:0; left:0;
+  width: 100%;
+  height: 60px;
+  padding: 0 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: ${(props) => props.bg};
 `;
 
 const Logo = styled.div`
-    font-size: 28px;
-    font-weight: 700;
-    a{
-        color:crimson;
-    }
+  font-size: 28px;
+  font-weight: 700;
+  a {
+    color: crimson;
+  }
 `;
 
 const MenuWrap = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const Menu = styled.div`
-    font-weight:600;
-    margin-left:50px;
+  font-weight: 600;
+  margin-left: 50px;
 `;
 
-
-
 export const Header = () => {
-    return <SHeader>
-        <Logo>
-            <Link to="/">PATI</Link>
-        </Logo>
+  const [bg, setBg] = useState("");
 
-        <MenuWrap>
-            <Menu>
-                <Link to="/">홈</Link>
-            </Menu>
-            <Menu>
-                <Link to="/search">탐색하기</Link>
-            </Menu>
-        </MenuWrap>
+  const scrollHandler = () => {
+    const sct = window.pageYOffset;
 
+    if (sct >= 200) {
+      setBg("#1d1d1d");
+    } else {
+      setBg("transparent");
+    }
+  };
+
+  window.addEventListener("scroll", scrollHandler);
+
+  return (
+    <SHeader bg={bg}>
+      <Logo>
+        <Link to="/">PATI</Link>
+      </Logo>
+
+      <MenuWrap>
+        <Menu>
+          <Link to="/">홈</Link>
+        </Menu>
+        <Menu>
+          <Link to="/search">탐색하기</Link>
+        </Menu>
+      </MenuWrap>
     </SHeader>
-}
+  );
+};
--- src/components/Section.js
+++ src/components/Section.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const SSection = styled.section`
   padding: 0 80px;
+  height: 200vh;
 `;
 
 export const Section = ({ children }) => {
